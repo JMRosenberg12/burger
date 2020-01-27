03ede3c76c482c247e550739d67ae93b2c3d1de5
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-01-27 09:47:56 -0600

    edit sql files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Son of Baconator', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Barbecue Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon BBQ Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder with Cheese Bacon', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Double Cheeseburger', true);

4cee28e5cc8478255598f5893f78baa7d9263000
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-01-27 09:34:09 -0600

    add db folder with 2 sql files

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ connection.connect(function(err) {
   });
 
   app.get("/", function(req, res) {
-    connection.query("SELECT * FROM burger;", function(err, data) {
+    connection.query("SELECT * FROM burgers;", function(err, data) {
       if (err) {
         return res.status(500).end();
       }
@@ -43,9 +43,81 @@ connection.connect(function(err) {
   });
 
   app.get("/:id", function(req, res) {
-    connection.query("SELECT * FROM burger where id = ?", [req.params.id], function(err, data) {
+    connection.query("SELECT * FROM burgers where id = ?", [req.params.id], function(err, data) {
       if (err) {
         return res.status(500).end();
       }
+      res.render("index", { burgers: data });
+    });
+  });
+
+  app.get("/:id", function(req, res) {
+    connection.query("SELECT * FROM burgers where id = ?", [req.params.id], function(err, data) {
+      if (err) {
+        return res.status(500).end();
+      }
+  
+      console.log(data);
+      res.render("single-burger", data[0]);
+    });
+  });
+
+  app.post("/api/burger", function(req, res) {
+    connection.query("INSERT INTO burger, [req.params.id] function(
+      err,
+      result
+    ) {
+      if (err) {
+        // If an error occurred, send a generic server failure
+        return res.status(500).end();
+      }
+  
+      // Send back the ID of new burger
+      res.json({ id: result.insertId });
+    });
+  });
+  app.delete("/api/quotes/:id", function(req, res) {
+    connection.query("DELETE FROM quotes WHERE id = ?", [req.params.id], function(err, result) {
+      if (err) {
+        // If an error occurred, send a generic server failure
+        return res.status(500).end();
+      }
+      else if (result.affectedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+  
+    });
+  });
+
+  app.put("/api/quotes/:id", function(req, res) {
+    connection.query(
+      "UPDATE quotes SET author = ?, quote = ? WHERE id = ?",
+      [req.body.author, req.body.quote, req.params.id],
+      function(err, result) {
+        if (err) {
+          // If an error occurred, send a generic server failure
+          return res.status(500).end();
+        }
+        else if (result.changedRows === 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+        }
+        res.status(200).end();
+  
+      }
+    );
+  });
+  
+  app.listen(PORT, function() {
+    
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
+  
+  
+  
+  
   
   
